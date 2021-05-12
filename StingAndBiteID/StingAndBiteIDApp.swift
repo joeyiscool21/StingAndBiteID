41d336eae81303df52cc1bc5df9efe4d7b7dbfe8
Joey <jserenko@runatek.com>
2021-05-11 21:21:00 -0500

    Initial Commit

---
+++ StingAndBiteID/StingAndBiteIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  StingAndBiteIDApp.swift
+//  StingAndBiteID
+//
+//  Created by joey serenko on 5/11/21.
+//
+
+import SwiftUI
+
+@main
+struct StingAndBiteIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
